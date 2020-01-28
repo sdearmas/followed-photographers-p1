75db3471e8ae05905404bca5f13151bc9677be8d
sdearmas <59620282+sdearmas@users.noreply.github.com>
2020-01-28 09:13:13 -0800

    Project 1 Week 4 Update

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 // your script file for adding your own jquery
 $(function() {
 // Your Code from here on down. Don't delete that line above!
-  $('details').hide();
+$('.details').click(function() {
+  $(this).toggleClass('details-active')
+
+  // $(".details").click(function() {
+  //   $(this).toggleClass('sub-nav-active')
+});
 
 
 
 
-
-
-
-
-
+// $('.photographers:nth-child(1)').click(function(){
+//   $('.photographers:nth-child(1)').toggleClass('details-active')
+// })
 
 
 
