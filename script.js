d4cbf6afc5492dc00eaff21d7ed3589213c1509c
sdearmas <59620282+sdearmas@users.noreply.github.com>
2020-02-04 13:19:56 -0800

    Slick Add-on 4 Bob

--- script.js
+++ script.js
@@ -5,5 +5,47 @@ $('.details').click(function() {
   $('.details').not(this).toggleClass('sub-nav-active')
   $(this).toggleClass('details-active')
 });
+
+$('.my-slick').slick({
+  dots: false,
+  infinite: true,
+  speed: 100,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2500,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+      	centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+      	centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+      	centerMode: true,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
 // End of Your Code . Don't delete that line below!!
 });
