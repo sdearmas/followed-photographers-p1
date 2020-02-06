a5e2dd393ccc56a192890994c5ba60193b808054
sdearmas <59620282+sdearmas@users.noreply.github.com>
2020-02-06 11:52:08 -0800

    2/6 Push Post Class

--- script.js
+++ script.js
@@ -3,48 +3,20 @@ $(function() {
 // Your Code from here on down. Don't delete that line above!
 $('.details').click(function() {
   $('.details').not(this).toggleClass('sub-nav-active')
-  $(this).toggleClass('details-active')
+  $(this).toggleClass('details-active').toggleclass('bob')
 });
 
 $('.my-slick').slick({
   dots: false,
   infinite: true,
-  speed: 3000,
+  speed: 3500,
   slidesToShow: 1,
   slidesToScroll: 1,
   autoplay: true,
-  autoplaySpeed: 2500,
-  responsive: [
-    {
-      breakpoint: 1024,
-      settings: {
-      	centerMode: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 600,
-      settings: {
-      	centerMode: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    },
-    {
-      breakpoint: 480,
-      settings: {
-      	centerMode: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-      }
-    }
+  autoplaySpeed: 2500
     // You can unslick at a given breakpoint now by adding:
     // settings: "unslick"
     // instead of a settings object
-]
 });
 
 // End of Your Code . Don't delete that line below!!
